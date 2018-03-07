2bcb9be23c78eaeeff8a2a0018252ad4d57f0eb0
witek1902 <witek1902@gmail.com>
2018-03-07 21:31:47 +0100

    Add axios.

---
+++ src/components/http/api-client.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const HTTP = axios.create({
+  baseURL: 'http://localhost:8081/api',
+  headers: {
+    'Access-Control-Allow-Origin': 'http://localhost:8080',
+  }
+});
