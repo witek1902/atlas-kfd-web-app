06aafce2bf22e2435ff5adf07490c1755ab97efe
witek1902 <witek1902@gmail.com>
2018-03-07 21:32:22 +0100

    Add Home site with example button click action.

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <img src="../assets/logo-kfd.png" />
+    <el-button type="primary" @click="changeTitle">Change title</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home ',
+  data () {
+    return {
+      title: 'Inżynierskie Targi Pracy - Atlas ćwiczeń',
+      anotherTitle: 'Atlas ćwiczeń'
+    }
+  },
+  methods: {
+    changeTitle () {
+      let newTitle = this.anotherTitle; 
+      this.anotherTitle = this.title;
+      this.title = newTitle;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
