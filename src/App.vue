54f366f360172fa66d2f467aebb1a74b5259cd88
witek1902 <witek1902@gmail.com>
2018-03-07 21:31:37 +0100

    Add main template with navigation and base styles.

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <nav class="main-nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/exerciseSections">Exercise sections</router-link>
+    </nav>
+    <div id="app-container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#app-container {
+  margin-top: 10px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.main-nav {
+  background-color:#42b983;
+  padding: 10px;
+  height: 20px;
+}
+
+.main-nav > a {
+  color: white;
+  text-decoration: none;
+  margin-right: 20px;
+}
+</style>
