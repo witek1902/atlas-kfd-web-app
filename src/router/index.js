27d735f9064d5788dd8eef91ce262958290a4f48
witek1902 <witek1902@gmail.com>
2018-03-07 21:32:03 +0100

    Add vue-router config.

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+
+import ExerciseSectionList from '@/components/exerciseSections/ExerciseSectionList';
+import ExerciseSectionDetails from '@/components/exerciseSections/ExerciseSectionDetails';
+import ExerciseDetails from '@/components/exercises/ExerciseDetails';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/exerciseSections',
+      name: 'ExerciseSectionList',
+      component: ExerciseSectionList
+    },
+    {
+      path: '/exerciseSections/:sectionId',
+      name: 'ExerciseSectionDetails',
+      component: ExerciseSectionDetails,
+      props: true
+    },
+    {
+      path: '/exercises/:exerciseId',
+      name: 'ExerciseDetails',
+      component: ExerciseDetails,
+      props: true
+    }
+  ]
+});
